e539f00b86b85626a455d17d295f450e492e90fe
Kushal425 <kushalsarkar457@gmail.com>
2026-01-29 20:04:51 +0530

    dashboard_working

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,20 @@
-import { PlusIcon, UploadCloudIcon } from 'lucide-react'
-import React from 'react'
+import { FilePenLineIcon, PlusIcon, UploadCloudIcon } from 'lucide-react'
+import React, { use } from 'react'
+import { dummyResumeData } from '../assets/assets'
 
 const Dashboard = () => {
+
+    const colors = ['#9333ea','#d97706','#dc2626','#0284c7','#16a34a']
+    const [allResumes, setAllResumes] = useState([])
+
+    const loadAllResumes = async () => {
+        setAllResumes(dummyResumeData)
+    }
+
+    useEffect(() => {
+        loadAllResumes()
+    }, [])
+
   return (
     <div>
         <div className='max-w-7xl mx-auto px-4 py-8'>
@@ -19,6 +32,19 @@ const Dashboard = () => {
             </div>
 
             <hr className='border-slate-300 my-6 sm:w-[305px]'/>
+
+            <div className="grid grid-cols-2 sm:flex flex-wrap gap-4">
+                {allResumes.map((resume,index) => {
+                    const baseColor = colors[index % colors.length]
+                    return (
+                        <button key={index} className="relative w-full sm:max-w-36 h-48 flex flex-col items-center justify-center rounded-lg gap-2 border group hover:shadow-lg transition-all duration-300 cursor-pointer" style={{background: `linear-gradient(135deg, ${baseColor}10, ${baseColor}40)`,borderColor: baseColor + '40'}}>
+                            <FilePenLineIcon className='size-7 group-hover:scale-105 transition-all' style={{color:baseColor}}/>
+                            <p>{resume.title}</p>
+                            <p>Updated on {new Date(resume.updatedAt).toLocaleDateString()}</p>
+                        </button>
+                    )
+                })}
+            </div>
         </div>
     </div>
   )
